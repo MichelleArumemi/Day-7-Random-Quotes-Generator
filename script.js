63fbcc177dcd79b38383991f58a42a6cca4e005e
MichelleArumemi <Arumemi.michelle@binghamuni.edu.ng>
2025-12-19 23:59:52 +0100

    updated

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
 const api_url = "https://api.quotable.io/random";
 
 async function getQuote(url) {
